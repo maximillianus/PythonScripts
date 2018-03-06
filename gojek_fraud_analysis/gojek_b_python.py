b1083dc8efa258dd930f55da803930562677a090
Pradana <pradana1@global.anz.com>
2018-03-06 20:39:17 +0800

    gojek

--- gojek_fraud_analysis/gojek_b_python.py
+++ gojek_fraud_analysis/gojek_b_python.py
@@ -1,15 +1,18 @@
 import os
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 ## Import data
 filename = 'gojek_problem_b.csv'
 dataset = []
 with open(filename, 'r') as f:
     lines = csv.reader(f)
-    dataset = list(lines)
-    # lines = f.readlines()
-    # lines = [l.replace('\n', '') for l in lines]
-    # dataset = (lines)
+    for row in lines:
+        dataset.append(row)
+    # alternative
+    # dataset = list(lines)
 
 print(dataset[0:3])
 agentName = [d[0] for d in dataset[1:]]
@@ -18,10 +21,11 @@ driverID = [d[2] for d in dataset[1:]]
 
 ## Questions to answer
 
-# How many driverID being registered for each agent?
+# 1. How many driverID being registered for each agent?
 from collections import Counter
 counting = Counter(agentName)
 print(counting)
+
 
 """
 Ricky seems to have the most number of driver registered.
@@ -29,12 +33,77 @@ Mamat comes in second.
 Bayu comes in 3rd.
 """
 
-# What is common registration period for each agent?
+# 2. What is common registration period for each agent?
 
 ### let's first clean timestamp
 from datetime import datetime
 timeStamp = [t + '00' for t in timeStamp]
 timeStamp = [datetime.strptime(t, '%Y-%m-%d %H:%M:%S.%f%z') for t in timeStamp]
-timeStamp_date = timeStamp[0].date()
-print(type(timeStamp_date))
+timeStamp_date = [t.date() for t in timeStamp]
+timeStamp_dayname = [t.date().strftime('%A') for t in timeStamp]
+timeStamp_time = [t.time().strftime('%H:%M:%S') for t in timeStamp]
+timeStamp_hour = [t[0:2] for t in timeStamp_time]
+timeStamp_min = [t[3:5] for t in timeStamp_time]
+timeStamp_sec = [t[7:10] for t in timeStamp_time]
+
+# a. most common reg hours
+hourCounter = Counter(timeStamp_hour)
+#print(hourCounter)
+
+dfhour = pd.DataFrame({'agent':agentName,
+                       'hour':timeStamp_hour})
+
+# shows frequency distribution of agentname vs hour
+#print(pd.crosstab(index=dfhour.agent, columns=dfhour.hour))
+
+# try to show top registration count and its hours
+dfhour = dfhour.groupby(['agent', 'hour']).size().reset_index(name='count')
+#print(dfhour.loc[dfhour.groupby(['agent'])['count'].idxmax()])
+
+"""
+- 4 out of 6 people commonly registers driver at 11AM
+- 2 out of 6 people registers people the most at 11PM
+- 11PM is way after working hours. Regist qty should not be high
+"""
+
+# b. try to check the registration days
+dayorder = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+            'Friday', 'Saturday', 'Sunday']
+
+dfday = pd.DataFrame({'agent':agentName,
+                      'date': timeStamp_date,
+                      'dayname': timeStamp_dayname})
+dfday.dayname = dfday.dayname.astype('category')
+dfday.dayname.cat.reorder_categories(dayorder,inplace=True)
+
+#print(dfday.head())
+#print(pd.crosstab(index=dfday.agent, columns=dfday.dayname))
+
+"""
+Interestingly, Mamat & Ricky are registering drivers in weekends too!
+Ricky has been registering agents at higher rate compared to his peer.
+Mamat has been registering agents at much lower rate compared to his peer.
+"""
+
+# c. try to check time between registration.
+# what is the registration duration?
+
+# 3. Is there possible erroneous entry of driverID?
+
+# let's look at some sample of driverID
+#print('DriverID:', driverID[:5])
+# driverID has 7 digits, started with '1' mostly.
+
+# a. is there any wrong driverID?
+#print('Check length of driverID')
+errorlength = [[i,len(x)] for i,x in enumerate(driverID) if len(x) > 7]
+# No length is beyond 7
+
+# b. is there any funny driverID pattern?
+# let's sort driverID for each agent and find if any ID is entered
+# consecutively
+dfdriver = pd.DataFrame({'agent': agentName,
+                         'driverID': driverID})
+print(dfdriver.head())
+
 
--- gojek_fraud_analysis/gojek_c_python.py
+++ gojek_fraud_analysis/gojek_c_python.py
@@ -1,23 +1,61 @@
 import os
 import csv
 import pandas as pd
+import datetime
 
 print(os.getcwd())
-print(os.listdir())
 
 ## Read data
 filename = 'gojek_problem_c.csv'
 df = pd.read_csv(filename)
-print(df.head())
 
-## Data Exploration
 
-# unique customer
-uni_cust_id = df.customer_id.unique()
-uni_cust_name = df.customer_name.unique()
-print(len(uni_cust_id))
-print(uni_cust_name)
+## Data Cleaning
 
-print(df.shopping_estimated_price.describe())
+# convert integer to date for data_date
+df.data_date = [int(i) for i in df.data_date]
+df.data_date = [(datetime.datetime(1899,12,30) + datetime.timedelta(days=int(t))) for t in df.data_date]
 
+# convert booking_date, dispatch_time, closing_time to datetime object
+df.booking_date = [pd.to_datetime(t).tz_localize('UTC').tz_convert('Asia/Jakarta') for t in df.booking_date]
+df.dispatch_time = [pd.to_datetime(t).tz_localize('UTC').tz_convert('Asia/Jakarta') for t in df.dispatch_time]
+
+# When converting dispatch time to closing time, there is unusual datetime pattern observed
+# observed 24:16:10 instead of 00:16:10. Need to investigate further on this
+
+# ** unusual closing_time format **
+unusual_closing_time_format = [ [i, t[11:19]] for i, t in enumerate(df.closing_time) if t[11:13] == '24']
+print(unusual_closing_time_format)
+
+# format to correct one
+df.closing_time = [t.replace(' 24:',' 00:') for t in df.closing_time]
+df.closing_time = [pd.to_datetime(t).tz_localize('UTC').tz_convert('Asia/Jakarta') for t in df.closing_time]
+
+
+## Investigation
+
+# Check how many booking/dispatch/closing per date
+df.booking_date.dt.strftime('%Y-%m-%d').value_counts(sort=False).sort_index()
+df.dispatch_time.dt.strftime('%Y-%m-%d').value_counts(sort=False).sort_index()
+df.closing_time.dt.strftime('%Y-%m-%d').value_counts(sort=False).sort_index()
+
+# Check how many booking/dispatch/closing per time
+df.booking_date.dt.strftime('%H-%M-00').value_counts(sort=False).sort_index()
+df.dispatch_time.dt.strftime('%H-%M-00').value_counts(sort=False).sort_index()
+df.closing_time.dt.strftime('%H-%M-00').value_counts(sort=False).sort_index()
+
+# Check time difference between book - dispatch
+# ** There is a lot of booking dispatched within 5 seconds or less
+book_to_dispatch = df.dispatch_time - df.booking_date
+print(book_to_dispatch.value_counts().head(10))
+
+# Check time difference between dispatch - close
+# ** There is a lot of booking closed within 20 seconds or less
+# ** It does not make sense booking can be closed within seconds
+# but probably there is short distance
+dispatch_to_close = df.closing_time - df.dispatch_time
+print(dispatch_to_close.value_counts().head(10))
+
+df['book_to_dispatch'] = book_to_dispatch
+df['dispatch_to_close'] = dispatch_to_close
 
